26e09764da913029b278844fc98f00f98a5be05b
Martyna <marsze007@wp.pl>
2020-05-01 23:53:08 +0200

    Updates

---
+++ precache-manifest.47fccb9851b1916c7cd4a45e4f7158d2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0182ff8c1268800839fd299c9ecdf614",
+    "url": "/memoryGame/index.html"
+  },
+  {
+    "revision": "ffb2ccc30f63366f4ecb",
+    "url": "/memoryGame/static/css/main.c52e5887.chunk.css"
+  },
+  {
+    "revision": "c3c7e45c9a57d6795c78",
+    "url": "/memoryGame/static/js/2.1215ac53.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/memoryGame/static/js/2.1215ac53.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ffb2ccc30f63366f4ecb",
+    "url": "/memoryGame/static/js/main.1090246e.chunk.js"
+  },
+  {
+    "revision": "093b80ecdf9ef995c646",
+    "url": "/memoryGame/static/js/runtime-main.58ee7846.js"
+  }
+]);
