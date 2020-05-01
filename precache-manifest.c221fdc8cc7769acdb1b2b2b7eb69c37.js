e97259db36b2df8a18ae85728f06f88ee3a864ac
Martyna <marsze007@wp.pl>
2020-05-01 23:12:05 +0200

    Commit for github.io

---
+++ precache-manifest.c221fdc8cc7769acdb1b2b2b7eb69c37.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "937331936631d3c255684c90eccbcd30",
+    "url": "/index.html"
+  },
+  {
+    "revision": "aa38eb271e6de80a84b2",
+    "url": "/static/css/main.c52e5887.chunk.css"
+  },
+  {
+    "revision": "c3c7e45c9a57d6795c78",
+    "url": "/static/js/2.1215ac53.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.1215ac53.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aa38eb271e6de80a84b2",
+    "url": "/static/js/main.b45ed293.chunk.js"
+  },
+  {
+    "revision": "1a11a102e6f46fde0321",
+    "url": "/static/js/runtime-main.54f136f8.js"
+  }
+]);
